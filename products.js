2a978b12faf312963aa9b7473122a6d45e4a9a0d
Onwaba <onwaba@projectcodex.co>
2016-06-14 12:13:30 +0200

    Add gitignore for node_modules folder

---
+++ products.js
@@ -0,0 +1,205 @@
+var fs = require('fs');
+	exports.salesData = function(filePath){
+		var readFile = fs.readFileSync(filePath,"utf8");
+		//console.log("==========" + fs.readFileSync('csv-files/week1.csv', 'utf8'));
+		lines = readFile
+		.replace("Day,Date,stock item,No sold,Sales Price\n", "")
+		.split('\n');
+
+		var productList = [];
+		var products = []
+
+		for(var i = 0; i < lines.length - 1;i++){
+			products.push(lines[i]);
+		}
+
+		products.forEach(function(product){
+			var columns = product.split(",");
+			var productName = columns[2];
+			var quantity = columns[3];
+			var sellingPrice = Number(columns[4].replace("R","").replace(",","."));
+			
+
+			var prodObj = {
+				productName:productName,
+				quantity: Number(quantity),
+				sellingPrice: sellingPrice
+			};
+			productList.push(prodObj);
+		});
+		return productList;			
+	};
+	
+	exports.groupByQuantity = function(productList){
+	 	var productMap ={};
+
+	 	productList.forEach(function(product){
+	 		//console.log(product);
+	 		var item = product.productName;
+	 		var qty = product.quantity;
+
+	 		if(productMap[item] === undefined){
+	 			productMap[item] = 0;
+	 		}
+
+	 		productMap[item] = productMap[item] + qty;
+	 	});
+	 	//console.log(productMap);
+	 	return productMap;
+	};
+	exports.salesPerWeek = function(productList){
+		//console.log(productList);
+		var weeklySales = {};
+
+  		productList.forEach(function(sale) {
+  			var product = sale.productName;
+  			var sale = Number(sale.quantity * sale.sellingPrice)
+
+    		if (!weeklySales.hasOwnProperty(product)) {
+      			weeklySales[product] = 0;
+    		}
+      		weeklySales[product] += sale;
+  		});
+  		return weeklySales
+	};
+
+	exports.mostPopularProd = function(productMap){
+		var mostPopularProduct = {};
+		var max = 0;
+		for(key in productMap){
+			if(productMap[key] > max){
+				max = productMap[key];
+
+				mostPopularProduct = {
+					product : key,
+					quantity: max
+				};
+			}
+		}
+		return mostPopularProduct;
+	}
+	exports.leastPopularProd = function(productMap,max){
+		var leastPopularProduct = {};
+		var min = max;
+		for(key in productMap){
+			if(productMap[key] < min){
+				min = productMap[key];
+
+				leastPopularProduct = {
+					product : key,
+					quantity: min
+				};
+			}
+		}
+		return leastPopularProduct;
+	}
+	var productCategories = {
+ 			'Milk 1l': 'Dairy Products',
+ 			'Imasi': 'Dairy Products',
+ 			'Bread': 'Bakery',
+ 			'Chakalaka Can': 'Canned Food',
+ 			'Gold Dish Vegetable Curry Can': 'Canned Food',
+ 			'Fanta 500ml': 'Fizzy Drinks',
+ 			'Coke 500ml': 'Fizzy Drinks',
+ 			'Cream Soda 500ml': 'Fizzy Drinks',
+ 			'Iwisa Pap 5kg': 'Bulk',
+ 			'Top Class Soy Mince': 'Soup',
+ 			'Shampoo 1 litre': 'Cosmetics',
+ 			'Soap Bar': 'Cosmetics',
+ 			'Bananas - loose': 'Fruit',
+ 			'Apples - loose': 'Fruit',
+ 			'Mixed Sweets 5s': 'Confectionery',
+ 			'Heart Chocolates': 'Confectionery',
+ 			'Rose (plastic)': 'Valentines Goodies',
+ 			'Valentine Cards': 'Valentines Goodies'
+ 		}
+	exports.mostPopularCat = function(productMap){
+ 		var catMap = {};
+
+ 		for (var product in productMap){
+ 			var category = productCategories[product];
+ 			if (catMap[category] === undefined){
+ 				catMap[category] = 0;
+ 			}
+ 			var prodQty = productMap[product];
+ 			catMap[category] = catMap[category] + prodQty; 
+ 		}
+        console.log(catMap);
+ 		var mostPopularCategory = {};
+ 		var max = 0;
+
+ 		for(var cat in catMap) {
+ 			var value = catMap[cat];
+ 			if(catMap[cat] > max){
+ 				max = catMap[cat];
+ 				mostPopularCategory = {
+ 					category: cat,
+ 					quantity: max
+ 				}
+ 			}
+ 		}
+ 		return mostPopularCategory;
+ 	};
+ 	exports.leastPopularCat = function(productMap,max){
+
+ 		
+ 		var catMap = {};
+
+ 		for (var product in productMap){
+ 			var category = productCategories[product];
+
+ 			if (catMap[category] === undefined){
+ 				catMap[category] = 0;
+ 			}
+ 			var prodQty = productMap[product];
+ 			catMap[category] = catMap[category] + prodQty; 
+ 		}
+
+ 		var leastPopularCategory = {};
+ 		var min = max;
+
+ 		for(var cat in catMap) {
+ 			var value = catMap[cat];
+ 			if(catMap[cat] < min){
+ 				min = catMap[cat];
+ 				leastPopularCategory = {
+ 					category: cat,
+ 					quantity: min
+ 				}
+ 			}
+ 		}
+ 		return leastPopularCategory;	
+ 	}
+ 	exports.weeklyPurchases = function(inputPurchases){
+
+ 		var purchasesArray = [];
+
+  		for (i = 0; i < inputPurchases.length - 1; i++) {
+    		purchasesArray.push(inputPurchases[i].split(";"));
+  		}
+
+  		for (var i = purchasesArray.length - 1; i >= 0; i--) {
+  			console.log(purchasesArray[i][1]);
+    		}
+  	}
+ 	exports.mostProfitableProd = function(productList,purchasesList){
+ 		var max = 0;
+ 		var mostProfitableProdObj
+ 		purchasesList.forEach(function(purchase){
+ 			productList.forEach(function(sale){
+ 				if(purchase.productName === sale.productName){
+ 					var profit = Number(sale.sellingPrice - purchase.purchasePrice);
+ 				}
+ 				if(profit > max){
+ 					max = profit;
+
+ 					mostProfitableProdObj = {
+ 						productName : purchase.item,
+ 						profitAmount : max
+ 					};
+ 				}
+ 			})
+ 		})
+ 		return mostProfitableProdObj
+
+ 	};
---
+++ purchases.js
@@ -0,0 +1,150 @@
+var fs = require('fs');
+exports.purchasesData = function(filePath){
+	var lines = fs.readFileSync(filePath, "utf8")
+  .replace("Shop;Date;Item;Quantity;Cost;Total Cost\n", "")
+  .replace(/,/g, ".")
+  .split('\n');
+
+  var purchasesArray = [];
+
+  for (i = 0; i < lines.length - 1; i++) {
+    purchasesArray.push(lines[i].split(";"));
+  }
+  var week0purchases = [];
+  var week1purchases = [];
+  var week2purchases = [];
+  var week3purchases = [];
+  var week4purchases = [];
+
+  purchasesArray.forEach(function(purchase){
+    var date = new Date(purchase[1] + '2016');
+
+    var endDate0 = new Date('01-Feb-2016');
+    var endDate1 = new Date('08-Feb-2016');
+    var endDate2 = new Date('15-Feb-2016');
+    var endDate3 = new Date('22-Feb-2016');
+    var endDate4 = new Date('02-Mar-2016');
+
+    if(date < endDate0){
+      week0purchases.push(purchase);
+    }
+    if(date > endDate0 && date < endDate1){
+      week1purchases.push(purchase);
+    }
+    if(date > endDate1 && date < endDate2){
+      week2purchases.push(purchase);
+    }
+    if(date > endDate2  && date < endDate3){
+      week3purchases.push(purchase);
+    }
+    if(date > endDate3 && date < endDate4){
+      week4purchases.push(purchase);
+    }
+  });
+
+  purchases = {
+    "week0": week0purchases,
+    "week1": week1purchases,
+    "week2": week2purchases,
+    "week3": week3purchases,
+    "week4": week4purchases
+  };
+  return (purchases);
+};
+exports.purchasesPerWeek = function(purchases,week){
+
+  var purchasesList = [];
+  purchases[week].forEach(function(array) {
+    purchasesList.push([array[2], Number(array[5].replace(/R/g,""))]);
+  });
+  var weeklyPurchases = {};
+
+  purchasesList.forEach(function(array) {
+
+    if (!weeklyPurchases.hasOwnProperty(array[0])) {
+      weeklyPurchases[array[0]] = 0;
+    }
+      weeklyPurchases[array[0]] += array[1];
+  });
+  return weeklyPurchases;	
+}
+
+exports.profitMapping = function(weeklyPurchases, weeklySales){
+	var profitMap = {};
+    var profit = {};
+
+ 	 for(product in weeklyPurchases){
+    for(key in weeklySales){
+      if(product === key){
+        profitMap[product] = weeklyPurchases[product] - weeklySales[key];
+      }
+    }
+   }
+   console.log(profitMap);
+ 	return profitMap;
+};
+
+exports.mostProfitableProduct = function(profitMap){
+  var profitObj = {};
+  var max = 0;
+
+  for(var key in profitMap){
+    if(profitMap[key] > max){
+      max = profitMap[key];
+      profitObj = {
+        productName : key,
+        qty : max
+      }
+    }
+  }
+  console.log(profitObj);
+  return profitObj;
+}
+
+exports.catProfit = function(profitMap){
+  var categoryProfitMap = { 'Milk 1l': 'Dairy Products',
+    Imasi: 'Dairy Products',
+    Bread: 'Bakery',
+    'Chakalaka Can': 'Canned Food',
+    'Gold Dish Vegetable Curry Can': 'Canned Food',
+    'Fanta 500ml': 'Fizzy Drinks',
+    'Coke 500ml': 'Fizzy Drinks',
+    'Cream Soda 500ml': 'Fizzy Drinks',
+    'Iwisa Pap 5kg': 'Bulk',
+    'Top Class Soy Mince': 'Soup',
+    'Shampoo 1 litre': 'Cosmetic',
+    'Soap Bar': 'Cosmetic',
+    'Bananas - loose': 'Fruit',
+    'Apples - loose': 'Fruit',
+    'Mixed Sweets 5s': 'Confectionery',
+    'Heart Chocolates': 'Confectionery',
+    'Rose (plastic)': 'Valentines Goodies',
+    'Valentine Cards': 'Valentines Goodies' 
+  };
+
+  var catProfitMap = {};
+
+  for (var product in profitMap){
+    var category = categoryProfitMap[product];
+    if (catProfitMap[category] === undefined){
+      catProfitMap[category] = 0;
+    }
+    var catProfit = profitMap[product];
+    catProfitMap[category] = catProfitMap[category] + catProfit; 
+  }
+  //console.log(catProfitMap);
+  var mostProfitableCategory = {};
+    var max = 0;
+
+    for(var cat in catProfitMap) {
+      var value = catProfitMap[cat];
+      if(catProfitMap[cat] > max){
+        max = catProfitMap[cat];
+        mostProfitableCategory = {
+          category: cat,
+          profit: max
+        }
+      }
+    }
+  return mostProfitableCategory;
+};
